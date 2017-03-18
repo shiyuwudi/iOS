e27b91c018989e6636ded17e297c71783e7ffc4f
shiyuwudi <289052669@qq.com>
2017-03-18 14:58:22 +0800

    first
    commit

---
+++ TestIOSCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestIOSCI
+//
+//  Created by apple2 on 2017/3/18.
+//  Copyright © 2017年 shiyuwudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
